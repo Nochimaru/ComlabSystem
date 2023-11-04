7a103eeaeed03560ba592c15b5ca60515d223e65
ricantiola <149871240+ricantiola@users.noreply.github.com>
2023-11-04 18:02:42 +0800

    Add files via upload

---
+++ Borrow.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<?php
+    include("itemconnection.php");
+
+    $SerialNum
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comlab Inventory System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styleUser.css">
+    <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="welcome">
+        <div class="test">
+            <table class="table my-2" style="background-color: white;">
+                <thead>
+                    <tr>
+                        <th scope="col">SERIAL_NUMBER</th>
+
+                        <th scope="col">ITEM_NAME</th>
+
+                        <th scope="col">ITEM_CLASSIFICATION</th>
+
+                        <th scope="col" >STATUS</th>
+
+                        <th scope="col">USER</th>
+
+                        <th scope="col">ACTION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $sql = "SELECT * FROM items;" ;
+                        $result = mysqli_query($con,$sql);
+                        $resultCheck = mysqli_num_rows($result);
+
+
+                        if($result){
+                            while($row=mysqli_fetch_assoc($result)){
+                            $SerialNum=$row['SerialNum'];
+                            $ItemName=$row['ItemName'];
+                            $Classification=$row['Classification'];
+                            $Availability = $row['Availability'];
+                            $UserID = $row['UserID'];
+                
+                            echo '<tr>
+                            <th scope="row">' . $SerialNum . '</th>
+                            <td>' . $ItemName . '</td>
+                            <td>' . $Classification . '</td>
+                            <td>' . $Availability . '</td>
+                            <td>' . $UserID . '</td>
+                            <td>
+                                <button class="btn btn-primary"><a href="EditItem.php?updateSerial=' . $SerialNum . '" class="text-light">Edit</a></button>
+                                <button class="btn btn-danger"><a href="DeleteItem.php?deleteSerial=' . $SerialNum . '" class="text-light">Remove</a></button>
+                            </td>
+                        </tr>';
+                    
+                        } 
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <button class="btn btn-success"><a href="Items.php" class="text-light">Add Device</a></button>
+
+        </div>
+    </div>
+    <div class="header">
+        <div class="side-nav">
+            <a href="Home.php" class="logo">
+            <img src="logo.png" class="logo-img">
+            <img src="logo2.png" class="logo-icon">
+            </a>
+            <ul class="nav-links">
+                <li><a href="Users.php"><i class="fa-solid fa-user"></i><p>USERS</p></a></li>
+                <li><a href="Items.php"><i class="fa-solid fa-paperclip"></i><p>ITEM REGISTRATION</p></a></li>
+                <li><a href="ItemTracking.php"><i class="fa-solid fa-eye"></i><p>ITEM TRACKING</p></a></li>
+                <li><a href=""><i class="fa-solid fa-chart-simple"></i><p>REPORTS</p></a></li>
+                <li><a href="logout.php"><i class="fa-solid fa-arrow-left"></i><p>LOGOUT</p></a></li>
+                <div class = "active"></div>
+            </ul>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ EditItem.php
@@ -0,0 +1,161 @@
+<?php
+  include 'itemconnection.php';
+
+  $itemNum = $_GET['updateItem'];
+  $sql = "SELECT * FROM items WHERE itemNum = $itemNum";
+  $result = mysqli_query($con, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  $SerialNum = $row['SerialNum'];
+  $ItemName = $row['ItemName'];
+  $Classification = $row['Classification'];
+  $Availability = $row['Availability'];
+  $UserID = $row['UserID'];
+
+  if(isset($_POST['submit'])){
+    $ItemName = $_POST['ItemName'];
+    $Classification = $_POST['Classification'];
+    $Availability = $_POST['Availability'];
+    $UserID = $_POST['UserID'];
+
+    $sql = "UPDATE items SET SerialNum = '$SerialNum', ItemName = '$ItemName', Classification = '$Classification', Availability = '$Availability', UserID = '$UserID' WHERE itemNum = $itemNum";
+    $result = mysqli_query($con, $sql);
+
+    if($result){
+      header('location: ItemTracking.php');
+    } else {
+      die(mysqli_error($con));
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comlab Inventory System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styleUser.css">
+    <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="help">
+      <form method="POST">
+
+        <div class="form-group">
+          <label>Item Name</label>
+          <input type="text" class="form-control" placeholder="Enter Item Name" name="ItemName" autocomplete="off" value="<?php echo $ItemName; ?>">
+        </div>
+
+        <div class="form-group">
+          <label>SerialNum</label>
+          <input type="text" class="form-control" placeholder="Enter Item Serial Number" name="SerialNum" autocomplete="off" value="<?php echo $SerialNum; ?>">
+        </div>
+
+        <div class="form-group">
+          <label>Classification</label>
+          <input type="text" class="form-control" placeholder="Enter Item Classification" name="Classification" autocomplete="off" value="<?php echo $Classification; ?>">
+        </div>
+
+        <div class="form-group">
+          <label>Availability</label>
+          <input type="text" class="form-control" placeholder="Enter Item Status" name="Availability" autocomplete="off" value="<?php echo $Availability; ?>">
+        </div>
+
+        <div class="form-group">
+          <label>UserID</label>
+          <input type="text" class="form-control" placeholder="Enter Item User" name="UserID" autocomplete="off" value="<?php echo $UserID; ?>">
+        </div>
+
+        <button type="submit" class="btn btn-primary" name="submit">Update</button>
+      </form>
+    </div>
+
+    <!-- Rest of your HTML code -->
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<?php
+    include("itemconnection.php");
+
+    $SerialNum
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comlab Inventory System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styleUser.css">
+    <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="help">
+      <form method="POST">
+
+      <div class="form-group">
+          <label>Item Name</label>
+          <input type="text" class="form-control" placeholder="Enter Item Name" name="ItemName" autocomplete="off" value=<?php echo $ItemName;?>>
+        </div>
+
+        <div class="form-group">
+          <label>SerialNum</label>
+          <input type="text" class="form-control" placeholder="Enter Item Serial Number" name="SerialNum" autocomplete="off" value=<?php echo $SerialNum;?>>
+
+        <div class="form-group">
+          <label>Classification</label>
+          <input type="text" class="form-control" placeholder="Enter Item Classification" name="Classification" autocomplete="off" value=<?php echo $Classification;?>>
+        </div>
+
+        <div class="form-group">
+          <label>Availability</label>
+          <input type="text" class="form-control" placeholder="Enter Item Status" name="Availability" autocomplete="off" value=<?php echo $Availability;?>>
+        </div>
+
+        <div class="form-group">
+          <label>UserID</label>
+          <input type="text" class="form-control" placeholder="Enter Item User" name="UserID" autocomplete="off" value=<?php echo $UserID;?>>
+
+        <?php
+           /*include 'loginconnection.php';
+           $query = mysqli_query($con,"Select * from login");
+           echo "<input type='search' list='dtlist' />";
+           echo "<datalist id='dtlist'>";
+            while($row = mysqli_fetch_array($query))
+            {
+                echo "<option>$row[UserID]</option>";
+            }
+            echo "<datalist>";*/ 
+        ?>   
+        </div>
+
+        <button type="submit" class="btn btn-primary" name="submit">Update</button>
+      </form>
+            
+  
+    </div>
+    <div class="header">
+        <div class="side-nav">
+            <a href="Home.php" class="logo">
+            <img src="logo.png" class="logo-img">
+            <img src="logo2.png" class="logo-icon">
+            </a>
+            <ul class="nav-links">
+                <li><a href="Users.php"><i class="fa-solid fa-user"></i><p>USERS</p></a></li>
+                <li><a href=""><i class="fa-solid fa-paperclip"></i><p>ITEM REGISTRATION</p></a></li>
+                <li><a href=""><i class="fa-solid fa-eye"></i><p>ITEM TRACKING</p></a></li>
+                <li><a href=""><i class="fa-solid fa-chart-simple"></i><p>REPORTS</p></a></li>
+                <li><a href="logout.php"><i class="fa-solid fa-arrow-left"></i><p>LOGOUT</p></a></li>
+                <div class = "active"></div>
+            </ul>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
